63c507be2fcd4007d6bf5c4e83596a31e110f1c4
woolf.no1 <woolf.no1@gmail.com>
2020-04-07 09:30:51 +0500

    "replace exception"

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,5 @@
 import math
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -31,5 +31,5 @@ class BasePage():
             alert_text = alert.text
             print(f"Your code: {alert_text}")
             alert.accept()
-        except NoAlertPresentException:
+        except TimeoutException:            
             print("No second alert presented")
