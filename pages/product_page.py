61700b656287841673d8c7f72064d85aef3dcf6a
woolf.no1 <woolf.no1@gmail.com>
2020-04-07 09:30:26 +0500

    "test with parametrize"

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,6 +3,8 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+    MESSAGE_SUCCESS_PRODUCT_ADD = "<strong>{product}</strong> has been added to your basket."
+    
     def add_product_to_basket(self):
         add_button = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         add_button.click()
@@ -17,8 +19,8 @@ class ProductPage(BasePage):
     def should_be_message_add_to_basket(self):
         assert self.is_element_present(*ProductPageLocators.MESSAGE_ADD_TO_BASKET), "Messsage about add product is not present"
 
-        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ADD_TO_BASKET).get_attribute("innerHTML").strip()
-        assert self.get_product_name() in message, "Product does not in message"
+        message = self.browser.find_element(*ProductPageLocators.MESSAGE_ADD_TO_BASKET).get_attribute("innerHTML").strip()        
+        assert self.__class__.MESSAGE_SUCCESS_PRODUCT_ADD.format(product=self.get_product_name())==message, "Product does not in message"
 
     def should_be_message_amount_basket(self):
         assert self.is_element_present(*ProductPageLocators.MESSAGE_AMOUNT_BASKET), "Message about amount basket is not present"
